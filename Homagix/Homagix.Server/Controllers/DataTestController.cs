2fe078703f70085ea07705a6ea4b40f2bc27d95a
Quajak <jasper.gerigk@gmx.net>
2018-09-30 20:41:06 +0200

    Added test if Azure has persistant storage

---
+++ Homagix/Homagix.Server/Controllers/DataTestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System.IO;
+using System.Text;
+
+namespace Homagix.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataTestController : Controller
+    {
+        [HttpGet("[action]")]
+        public IEnumerable<string> Data()
+        {
+            using (FileStream f = System.IO.File.Open("test.txt", FileMode.Append))
+            {
+                f.Write(Encoding.UTF8.GetBytes("1"));
+            }
+            string text = System.IO.File.ReadAllText("test.txt");
+            Log.Information(text + " in file!");
+            return new List<string>() { text };
+        }
+    }
+}
