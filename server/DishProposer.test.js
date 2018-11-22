741428c67fa7bcbfd9356bc0d30b1fc5bc6c058e
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-11-22 11:35:34 +0100

    Moved from mocha to jest

--- server/DishProposer.test.js
+++ server/DishProposer.test.js
@@ -14,13 +14,13 @@ class MockModel {
 }
 
 describe('DishProposer', () => {
-  it('should propose 7 dishes', done => {
+  test('should propose 7 dishes', done => {
     const proposer = new DishProposer({model: new MockModel()})
     proposer.get().map(d => d.id).should.deepEqual([1, 8, 12, 17, 23, 25, 29])
     done()
   })
 
-  it('should propose 7 dishes with some ids inhibited', done => {
+  test('should propose 7 dishes with some ids inhibited', done => {
     const proposer = new DishProposer({model: new MockModel()})
     proposer.get({inhibit: [12, 1, 33]}).map(d => d.id).should.deepEqual([8, 17, 23, 25, 29, 43, 44])
     done()
