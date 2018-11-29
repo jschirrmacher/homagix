746c2c7aece751e3a6c6ddcbac20044da5d27683
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-11-29 22:24:48 +0100

    Fixed tests

--- server/DishProposer.test.js
+++ server/DishProposer.test.js
@@ -5,7 +5,7 @@ const DishProposer = require('./DishProposer')
 
 class MockModel {
   constructor() {
-    this.ids = [1, 8, 12, 17, 23, 25, 29, 43, 44].map(id => ({id}))
+    this.ids = [1, 8, 12, 17, 23, 25, 29, 43, 44].map(id => ({id, ingredients: []}))
   }
 
   getDishes(num) {
@@ -14,15 +14,15 @@ class MockModel {
 }
 
 describe('DishProposer', () => {
-  test('should propose 7 dishes', done => {
+  it('should propose 7 dishes', done => {
     const proposer = new DishProposer({model: new MockModel()})
-    proposer.get().map(d => d.id).should.deepEqual([1, 8, 12, 17, 23, 25, 29])
+    proposer.get().dishes.map(d => d.id).should.deepEqual([1, 8, 12, 17, 23, 25, 29])
     done()
   })
 
-  test('should propose 7 dishes with some ids inhibited', done => {
+  it('should propose 7 dishes with some ids inhibited', done => {
     const proposer = new DishProposer({model: new MockModel()})
-    proposer.get({inhibit: [12, 1, 33]}).map(d => d.id).should.deepEqual([8, 17, 23, 25, 29, 43, 44])
+    proposer.get({inhibit: [12, 1, 33]}).dishes.map(d => d.id).should.deepEqual([8, 17, 23, 25, 29, 43, 44])
     done()
   })
 })
