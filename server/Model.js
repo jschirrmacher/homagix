84db18c24bbe829791d96bdefd1e1896601d7bd5
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-12-02 13:56:54 +0100

    Removed migration code

--- server/Model.js
+++ server/Model.js
@@ -30,41 +30,8 @@ class Model {
       dishes: [],
       ingredients: []
     }
-    const pathToStorage = path.join(basePath, 'speisen.yaml')
-    if (fs.existsSync(pathToStorage)) {
-      this.migrateData(YAML.parse(fs.readFileSync(pathToStorage).toString()))
-      fs.unlinkSync(pathToStorage)
-    } else {
-      this.events = JSON.parse(fs.readFileSync(path.join(basePath, 'events.json')))
-    }
+    this.events = JSON.parse(fs.readFileSync(path.join(basePath, 'events.json')))
     this.replay()
-  }
-
-  migrateData(data) {
-    const ingredients = []
-    let id = 0
-    data.dishes.forEach(dish => {
-      this.events.push({type: 'dish-added', id: dish.id, name: dish.name, source: dish.source})
-      if (dish.last) {
-        this.events.push({type: 'served', dish: dish.id, date: dish.last})
-      }
-      dish.ingredients.forEach(ingredient => {
-        let [amount, unit, ...nameComponents] = ingredient.split(' ')
-        amount = +amount.replace(',', '.')
-        if (knownUnits.indexOf(unit) === -1) {
-          throw `Unknown unit '${unit}'`
-        }
-        const name = nameComponents.join(' ')
-        let existing = ingredients.find(which => which.name === name)
-        if (!existing) {
-          existing = {type: 'ingredient-added', id: ++id, name}
-          this.events.push(existing)
-          ingredients.push({id, name})
-        }
-        this.events.push({type: 'ingredient-assigned', dish: dish.id, ingredient: existing.id, amount, unit})
-      })
-    })
-    fs.writeFileSync(path.join(basePath, 'events.json'), JSON.stringify(this.events, null, 2))
   }
 
   replay() {
