782823bc4ab967534d9b582c86a56df87dfdf65b
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-12-02 11:55:40 +0100

    Frontend for storing last served date

--- src/components/ProposalsList.js
+++ src/components/ProposalsList.js
@@ -1,40 +1,20 @@
 import React, { Component } from 'react'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
-import {getProposals} from '../actions/proposalsActions'
-
-const selected = obj => Object.keys(obj).filter(k => obj[k])
+import {getProposals, discardProposal, toggleProposalAcceptance} from '../actions/proposalsActions'
 
 class ProposalsList extends Component {
   constructor(props) {
     super(props)
     this.props.getProposals()
-    this.state = {
-      inhibit: {},
-      accepted: {}
-    }
-  }
-
-  removeProposal(item) {
-    const inhibit = this.state.inhibit
-    inhibit[item.id] = item
-    this.setState({inhibit})
-    this.props.getProposals(selected(inhibit), selected(this.state.accepted))
-  }
-
-  acceptProposal(item) {
-    const accepted = this.state.accepted
-    accepted[item.id] = !accepted[item.id]
-    this.setState({accepted})
-    this.props.getProposals(selected(this.state.inhibit), selected(accepted))
   }
 
   render() {
     const items = this.props.proposals && this.props.proposals.map(item => (
-      <li key={item.id} className={this.state.accepted[item.id] ? 'accepted' : ''}>
-        <button className="delete" onClick={() => this.removeProposal(item)} title="Reject proposal">&times;</button>
+      <li key={item.id} className={this.props.accepted[item.id] ? 'accepted' : ''}>
+        <button className="delete" onClick={() => this.props.discardProposal(item)} title="Reject proposal">&times;</button>
         {item.name}
-        <button className="accept" onClick={() => this.acceptProposal(item)} title="Accept proposal">&#10003;</button>
+        <button className="accept" onClick={() => this.props.toggleProposalAcceptance(item)} title="Accept proposal">&#10003;</button>
       </li>
     ))
 
@@ -44,12 +24,13 @@ class ProposalsList extends Component {
 
 function mapStateToProps(state) {
   return {
-    proposals: state.proposals.dishes
+    proposals: state.proposals.dishes,
+    accepted: state.proposals.accepted || {}
   }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({getProposals}, dispatch)
+  return bindActionCreators({getProposals, discardProposal, toggleProposalAcceptance}, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProposalsList)
