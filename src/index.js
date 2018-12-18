953595a0030fc919539f29832d52cd407bf33ddc
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-12-18 18:11:54 +0100

    Only need to start backend in development mode - frontend is served as well via webpack-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 import logo from './homagix.png'
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App'
+import 'babel-polyfill'
 
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import configureStore from './configureStore'
@@ -27,9 +27,6 @@ ReactDOM.render(
     </Provider>
   </React.Fragment>,
   document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+module.hot.accept();
