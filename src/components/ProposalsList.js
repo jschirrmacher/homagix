e8f143bb53acb808b5f45062aad119c91337576c
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-02-23 16:03:44 +0100

    Show date when dish was last served

--- src/components/ProposalsList.js
+++ src/components/ProposalsList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {getProposals, discardProposal, toggleProposalAcceptance} from '../actions/proposalsActions'
+import './ProposalsList.css'
 
 class ProposalsList extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class ProposalsList extends Component {
       <li key={item.id} className={this.props.accepted[item.id] ? 'accepted' : ''}>
         <button className="delete" onClick={() => this.props.discardProposal(item)} title="Reject proposal">&times;</button>
         {item.name}
+        <span className="last-served">{(new Date(item.last)).toLocaleDateString('de-DE', {day: '2-digit', month: '2-digit', year: 'numeric'})}</span>
         <button className="accept" onClick={() => this.props.toggleProposalAcceptance(item)} title="Accept proposal">&#10003;</button>
       </li>
     ))
