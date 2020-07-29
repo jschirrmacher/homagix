83ac4cbc24e43a6cf6c1584b44dcb79f11747642
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-07-29 09:35:36 +0200

    Removed unused script

--- migrate.js
+++
@@ -1,9 +0,0 @@
-const { readFileSync, createWriteStream } = require('fs')
-const { resolve } = require('path')
-
-const data = JSON.parse(readFileSync(resolve(__dirname, 'data', 'events.json')).toString())
-const output = createWriteStream(resolve(__dirname, 'data', 'events-0.json'))
-data.forEach(entry => {
-  output.write(JSON.stringify(entry) + '\n')
-})
-output.end()
