703d329a81cc9379b7877b4dd5bf6ea1437e5fd6
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-08-01 18:28:06 +0200

    Show ingredients of dishes

--- frontend/components/ShoppingList.vue
+++ frontend/components/ShoppingList.vue
@@ -1,9 +1,23 @@
 <script>
+import { mapState } from 'vuex'
+import IngredientList from './IngredientList'
+
 export default {
-  
+  components: { IngredientList },
+  computed: mapState(['shoppinglist'])
 }
 </script>
 
 <template>
-  <h2>Einkaufsliste</h2>
+  <div class="shoppinglist">
+    <h2>Einkaufsliste</h2>
+
+    <IngredientList :items="shoppinglist" />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.shoppinglist {
+  padding: 10px;
+}
+</style>
