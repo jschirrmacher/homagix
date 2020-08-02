c4d63c7d7c9c1ca3e8bca4d9c9623c4245e8a425
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-08-02 10:50:02 +0200

    Fixed parameter handling in API calls

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,12 +1,21 @@
 import { ERROR_OCCURED } from '@/store/mutation_types'
 
+function encodeParameter([key, val]) {
+  const value = val instanceof Array ? val.join(',') : val
+  return key + '=' + encodeURIComponent(value)
+}
+
+function prepareQueryParamers(params) {
+  return Object.entries(params).map(encodeParameter).join('&')
+}
+
 export async function doFetch(method, url, data) {
   const options = { method }
   if (data && !['get', 'options'].includes(method.toLowerCase())) {
     options.headers = {'content-type': 'application/json'}
     options.body = JSON.stringify(data)
   } else if (data && method.toLowerCase() === 'get') {
-    url += '?' + Object.values(data).map((val, key) => `${key}=${encodeURIComponent(val)}`).join('&')
+    url += '?' + prepareQueryParamers(data)
   }
   const response = await fetch(url, options)
   const content = response.headers.get('content-type').match(/json/) ? await response.json() : await response.text()
--- frontend/store/actions.js
+++ frontend/store/actions.js
@@ -1,19 +1,27 @@
 import * as types from './mutation_types'
-import { loadData } from '@/lib/api'
+import { loadData, doFetch } from '@/lib/api'
 
 export const actions = {
   [types.GET_PROPOSALS]: loadData('/proposals', types.PROPOSALS_LOADED),
+
   [types.GET_INGREDIENTS]: loadData('/ingredients', types.INGREDIENTS_LOADED),
+
   [types.DISH_ACCEPTED]: (context, { dishId }) => {
     context.commit(types.DISH_ACCEPTED, { dishId })
     loadData('/proposals', types.PROPOSALS_LOADED)(context)
   },
+
   [types.DISH_DECLINED]: (context, { dishId }) => {
     context.commit(types.DISH_DECLINED, { dishId })
     loadData('/proposals', types.PROPOSALS_LOADED)(context)
   },
-  [types.SHOPPING_DONE]: (context) => {
+
+  [types.SHOPPING_DONE]: async (context) => {
+    const data = {
+      accepted: context.state.accepted.join(','),
+      date: context.state.startDate.toISOString()
+    }
+    await doFetch('post', '/proposals/fix', data)
     context.commit(types.SHOPPING_DONE)
-    loadData('/proposals/fix', )
   }
 }
