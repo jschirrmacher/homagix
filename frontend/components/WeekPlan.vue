066d5a5eed45bb6a0e1d5f3b240aaca7da8db78a
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-08-11 18:46:49 +0200

    More compact

--- frontend/components/WeekPlan.vue
+++ frontend/components/WeekPlan.vue
@@ -24,8 +24,6 @@ export default {
 
 <template>
 <div>
-  <h2>Wochenplan beginnend ab <input type="date" v-model="startDate" autocomplete="off"></h2>
-
   <ul>
     <li v-for="proposal in proposals" :key="proposal.id">
       <Dish :id="proposal.id" :name="proposal.name" :lastServed="proposal.last" :ingredients="proposal.ingredients" />
@@ -35,20 +33,9 @@ export default {
 </template>
 
 <style scoped lang="scss">
-h2 input {
-  font-size: 1em;
-  font-family: Arial, helvetica, sans-serif;
-}
-
 @media (min-width: 640px) {
   ul {
     margin: 1em 0;
-  }
-}
-
-@media print {
-  input {
-    border: none;
   }
 }
 </style>
