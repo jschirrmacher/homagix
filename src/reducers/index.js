b441629909a83940563602057046b5a7a565f2aa
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-08-11 19:54:39 +0200

    Removed old frontend files

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux'
-import proposals from './proposalsReducer'
-import error from './errorReducer'
-
-const rootReducer = combineReducers({error, proposals})
-
-export default rootReducer
