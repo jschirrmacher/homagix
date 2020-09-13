97ef06e92999b861861b8702f1e2f3304b376bda
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-09-13 15:19:06 +0200

    Handle different ids of ingredients in files and event stream

--- server/models/ingredient.js
+++ server/models/ingredient.js
@@ -6,36 +6,46 @@ export default function ({ store, events }) {
     byName: {},
   }
 
+  const aliases = {}
+
   store
     .on(ingredientAdded, ({ id, name, unit }) => {
       const ingredient = { id, name, unit }
-      ingredients.byId[id] = ingredient
-      ingredients.byName[name.toLowerCase()] = ingredient
+      const existing = ingredients.byName[name.toLowerCase()]
+      if (existing) {
+        aliases[id] = existing.id
+      } else {
+        ingredients.byId[id] = ingredient
+        ingredients.byName[name.toLowerCase()] = ingredient
+      }
     })
     .on(ingredientUpdated, ({ ingredientId, name, value }) => {
-      const ingredient = ingredients.byId[ingredientId]
+      const ingredient = byId(ingredientId)
       ingredient[name] = value
     })
 
-  return {
-    getAll() {
-      return Object.values(ingredients.byId)
-    },
-
-    byId(id) {
-      return ingredients.byId[id]
-    },
-
-    byName(name) {
-      return ingredients.byName[name]
-    },
-
-    byExample(item) {
-      if (item.id) {
-        return this.byId(item.id)
-      }
-      const pattern = new RegExp('.*' + item.name + '.*')
-      return Object.values(ingredients.byId).find(i => i.name.match(pattern))
-    }
+  function getAll() {
+    return Object.values(ingredients.byId)
   }
+
+  function byId(id) {
+    if (aliases[id]) {
+      return ingredients.byId[aliases[id]]
+    }
+    return ingredients.byId[id]
+  }
+
+  function byName(name) {
+    return ingredients.byName[name]
+  }
+
+  function byExample(item) {
+    if (item.id) {
+      return this.byId(item.id)
+    }
+    const pattern = new RegExp('.*' + item.name + '.*')
+    return Object.values(ingredients.byId).find(i => i.name.match(pattern))
+  }
+
+  return { getAll, byId, byName, byExample }
 }
